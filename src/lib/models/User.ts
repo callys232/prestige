3691694baaa465663759b0c5175af0bfb757197a
samuel ibitoye <ibitoyesamuel25@gmail.com>
2025-10-14 18:30:36 +0100

    Update user

--- src/lib/models/User.ts
+++ src/lib/models/User.ts
@@ -8,16 +8,47 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String },
   username: { type: String, unique: true },
-  phoneNumber: { type: String, },
+  phoneNumber: { type: String },
   isVerified: { type: Boolean, default: false },
   accountDeleted: { type: Boolean, default: false },
   verificationCode: { type: String },
   refreshTokenJTI: { type: String, default: null },
   googleId: { type: String, unique: true, sparse: true },
   role: {
-    type: String, enum: ["admin", "seller", "client"], default: "client"
+    type: String, enum: ["admin", "trainer", "client"], default: "client"
   },
   joinedAt: { type: Date, default: Date.now },
+  membership: { type: String, enum: ["basic", "premium", "elite"] },
+  trainerId: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+  isActive: { type: Boolean, default: false },
+  gender: { type: String, enum: ["male", "female", "other"] },
+  medicalCondition: { type: String },
+  class: {
+    type: String,
+    enum: [
+      "dance fitness",
+      "zumba fusion",
+      "afro dance burn",
+      "kid fitness",
+      "junior bootcamp",
+      "mini movers",
+      "muscle marathon",
+      "endurance builder",
+      "hiit express",
+      "press to burn",
+      "cardio blast"
+    ]
+  },
+  goal: {
+    type: String,
+    enum: [
+      "build muscle",
+      "lose weight",
+      "improve endurance",
+      "increase flexibility",
+      "general fitness"
+    ]
+  },
 });
 
 export const User = mongoose.models.Users || mongoose.model("Users", UserSchema);
